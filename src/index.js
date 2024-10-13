14218bb1e203207d21d7ed31caf183e23d5af891
BryanWASD <brian.zaz.205.gh@gmail.com>
2024-10-12 20:27:10 -0600

    fix 2.0

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use('/api', contactRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI,)
     .then(() => console.log('Conectado a MongoDB'))
     .catch((error) => console.log(error));
 
